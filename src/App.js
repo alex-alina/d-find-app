3336c7505613e0d39bec7a2ff43addbf9c36bbea
pvdvpebble <pvdvpebble@gmail.com>
2018-11-06 18:00:11 +0100

    added routing feature - basic profile page component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import MainScreenContainer from './components/MainScreenContainer';
 import Navbar from './components/Navbar';
+import { Route } from 'react-router-dom';
+import UserProfilePage from './components/UserProfilePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainScreenContainer />
+        <Route exact path="/" component={MainScreenContainer}/>
+        <Route path="/userProfile/:id" component={UserProfilePage} />
         <Navbar />
       </div>
     );
