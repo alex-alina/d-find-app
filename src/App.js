a2e9c5fe08160bc1e66e5a1d678426b0ba39d7b7
pvdvpebble <pvdvpebble@gmail.com>
2018-11-06 14:02:32 +0100

    changed title header in app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Paul, I'm D.find</h1>
+        <h1>D.find</h1>
         <MainScreenContainer />
       </div>
     );
