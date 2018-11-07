a7cbcd29d4bf2668c2d3429e94a66458e8a71517
pvdvpebble <pvdvpebble@gmail.com>
2018-11-07 17:50:16 +0100

    added addmatch action and linked reducer to combiner

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import candidates from './candidates';
 import shownCandidate from './shownCandidate';
 import currentUser from './currentUser';
 import liked from './liked'
+import matched from './matched'
 
 export default combineReducers({
   candidates,
   shownCandidate,
   currentUser,
   liked,
+  matched,
 })
