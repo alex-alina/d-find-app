b515d963573fa86fed7bd71740f28cf282e7f1ab
pvdvpebble <pvdvpebble@gmail.com>
2018-11-07 13:25:10 +0100

    added showncandidate reducers - random candidate appearrs in redux state / added candidate database in seperate folder / updated candidates reducer initial state

--- src/components/MainScreenContainer.js
+++ src/components/MainScreenContainer.js
@@ -7,10 +7,7 @@ import ProfileText from './ProfileText';
 class MainScreenContainer extends React.Component {
  /*  state = { currentShown: {} }
 
- randomProfileSelector = () => {
-    console.log(Math.floor(Math.random() * (this.props.candidates.length)))
-    return Math.floor(Math.random() * (this.props.candidates.length))
-  }
+
 
   componentDidMount = () => {
     this.updateCurrentShown();
