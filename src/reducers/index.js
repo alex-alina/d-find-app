72d2bcd678f0023edc8122e697cbfaf2ee132ed2
pvdvpebble <pvdvpebble@gmail.com>
2018-11-07 11:49:52 +0100

    added candidate reducers, fixed import combineRededucers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,7 @@
 import { combineReducers } from 'redux'
 import candidates from './candidates'
-import currentUser from './currentUser'
-import liked from './liked'
-import matches from './matches'
-import passed from './passed'
 
 export default combineReducers({
   candidates,
-  currentUser,
-  liked,
-  matches,
-  passed,
+  
 })
