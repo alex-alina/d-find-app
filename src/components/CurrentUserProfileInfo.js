6ea416ec3f93b95fcd3df5029366149aa30716bd
pvdvpebble <pvdvpebble@gmail.com>
2018-11-08 16:15:42 +0100

    linked current user profile page

---
+++ src/components/CurrentUserProfileInfo.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import '../styles/CurrentUserInfo.css'
+export default function CurrentUserProfileInfo(props) {
+
+  return (<div className="info-container">
+      <h4>Name</h4>
+      <h4>E-Mail</h4>
+      <h4>Title</h4>
+      <h4>Location</h4>
+      <h4>Elevator Pitch</h4>
+      <h4>Previous Experiences</h4>
+      <h4>Elevator Pitch</h4>
+      <h4>Previous Experiences</h4>
+      <h4>Languages</h4>
+      <h4>Skills</h4>
+      <h4>Education</h4>
+  </div>)
+}
--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,8 +1,9 @@
+import Logo from "../images/FruitCompany.svg"
+
 const initialState = {
   id: "iam-logged-in@gmail.com",
   name: "The Fruit Company",
-  logo: "https://images.unsplash.com/photo-1521624002551-9ea092b0bc52?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9d3180f31423922e28f8f9b73ea95a5d&auto=format&fit=crop&w=1050&q=80",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue leo, eleifend eget risus at, sodales eleifend erat. Suspendisse in sodales erat. In hac habitasse platea dictumst. Quisque efficitur nulla at dignissim vehicula.",
+  logo: Logo,
   location: "Amsterdam"
 }
 
