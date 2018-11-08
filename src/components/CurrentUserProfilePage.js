93d514898696ccd3595ac3540710775b0d76399d
pvdvpebble <pvdvpebble@gmail.com>
2018-11-08 16:47:17 +0100

    added back button

--- src/components/CurrentUserProfilePage.js
+++ src/components/CurrentUserProfilePage.js
@@ -3,13 +3,16 @@ import { connect } from 'react-redux';
 import UserProfilePicture from './CurrentUserProfilePicture';
 import Navbar from './Navbar';
 import CurrentUserProfileInfo from './CurrentUserProfileInfo';
-import '../styles/UserProfile.css'
+import '../styles/CurrentUserProfilePage.css';
+import Backbutton from '../images/Back.svg';
+import { Link } from 'react-router-dom';
 
 
 class CurrentUserProfilePage extends React.Component {
 
   render() {
     return <div>
+      <Link to='/'><img src={Backbutton} alt="back-button" className="back-button"></img></Link>
       <div className="box">
         <UserProfilePicture currentUserProfile={this.props.currentUser} />
         <CurrentUserProfileInfo />
