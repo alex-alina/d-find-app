2b0a58bfc332a98f287ed153a70be5973acd45da
pvdvpebble <pvdvpebble@gmail.com>
2018-11-09 15:10:35 +0100

    profile selection is not random pass button works

--- src/actions/setShownCandidate.js
+++ src/actions/setShownCandidate.js
@@ -1,7 +1,8 @@
 export const SET_SHOWN_CANDIDATE = 'SET_SHOWN_CANDIDATE'
 
-export function setShownCandidate() {
+export function setShownCandidate(nextIndex) {
   return {
     type: 'SET_SHOWN_CANDIDATE',
+    payload: nextIndex,
   }
 }
